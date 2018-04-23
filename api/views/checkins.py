bf30429beba6dd8c8e18fefcf8175754487f76c9
Abdullahi Abdalla <abdalla6@illinois.edu>
2018-04-23 12:56:20 -0500

    Updated checkins to have all info

--- api/views/checkins.py
+++ api/views/checkins.py
@@ -67,8 +67,25 @@ def get_checkins_for_user(user_id):
             return jsonify({'status' : 'success', 'checkins' : checkins})
         except Exception as e:
             raise APIError(str(e))
+
     else:
         return jsonify({'status' : 'failed', 'message' : "Endpoint /checkin/<user_id> requires GET or POST request"})
+
+@mod.route('/checkin/id/<checkin_id>', methods=["DELETE"])
+def delete_checkins_with_id(checkin_id):
+    if request.method == "DELETE":
+        try:
+            result = conn.execute("DELETE FROM \"checkins\" WHERE checkin_id = {}".format(checkin_id))
+            result = conn.execute("SELECT * FROM \"checkins\" WHERE checkin_id = {}".format(checkin_id))
+
+            row = result.first()
+            if row != None:
+                return jsonify({'status' : 'failed', 'message' : "Failed to delete checkin"})
+            return jsonify({'status' : 'success', 'message' : "Deleted checkin!"})
+        except Exception as e:
+            raise APIError(str(e))
+    else:
+        return jsonify({'status' : 'failed', 'message' : "Endpoint /checkin/id/<checkin_id> requires DELETE request"})
 
 @mod.route('/checkin/location/<user_id>', methods=["GET"])
 def get_checkin_locations_for_user(user_id):
@@ -76,16 +93,16 @@ def get_checkin_locations_for_user(user_id):
         try:
             data = request.get_json()
             query = """
-            SELECT location.location_id, location.longitude, location.latitude, restaurant.restaurant_name
-            FROM location, restaurant,
-                (SELECT checkins.location_id
-                FROM location, checkins
-                WHERE "user".user_id = {0} AND
-                    "user".user_id = checkins.user_id
-                ) AS loc
-            WHERE location.location_id = loc.location_id AND
-                location.restaurant_id = restaurant.restaurant_id
-            """.format(user_id)
+                    SELECT location.location_id, location.longitude, location.latitude, restaurant.restaurant_name, loc.timestamp
+                    FROM location, restaurant,
+                        (SELECT checkins.location_id, checkins.timestamp
+                        FROM location, checkins, "user"
+                        WHERE "user".user_id = {0} AND
+                            "user".user_id = checkins.user_id
+                        ) AS loc
+                    WHERE location.location_id = loc.location_id AND
+                        location.restaurant_id = restaurant.restaurant_id
+                    """.format(user_id)
             result = conn.execute(query)
 
             checkins = []
